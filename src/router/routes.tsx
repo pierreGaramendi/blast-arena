b8bb66ac4aa34a74f5c2fddad9b642b67a92bed5
Pierre Garamendi <pierre.garamendi@gmail.com>
2024-10-13 20:06:49 -0500

    autoredirec was added

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import { App } from "../App";
 import { GameMenu } from "../modules/menu/Menu";
 import { FirstLevel } from "../modules/normal-game/first-level/first-level";
@@ -8,6 +8,10 @@ export const blastArenaRoutes = createBrowserRouter([
     path: "/",
     element: <App />,
     children: [
+      {
+        index: true,
+        element: <Navigate to="/menu" replace />, // Redirige automáticamente a "/menu"
+      },
       {
         path: "menu",
         element: <GameMenu />,
